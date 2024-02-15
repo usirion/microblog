9a5e94a64c04cf345c6e1368cf19c321d5b16b64
usirion <160060005+usirion@users.noreply.github.com>
2024-02-15 18:24:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,25 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
 
-def create_app():
-    app = Flask(__name__)
-    client = MongoClient(os.getenv("MONGODB_URI"))
-    app.db = client.microblog
 
-    @app.route("/", methods=["GET", "POST"])
-    def home():
-        if request.method == "POST":
-            entry_content = request.form.get("content")
-            formatted_date = datetime.datetime.today().strftime("%d-%m-%Y")
-            app.db.entries.insert({"content": entry_content, "date": formatted_date})
-        
-        entries_with_date = [
-            (
-                entry["content"],
-                entry["date"],
-                datetime.datetime.strptime(entry["date"], "%d-%m-%Y").strftime("%d-%m-%Y")
-            )
-            for entry in app.db.entries.find({})
-        ]
-        return render_template("home.html", entries=entries_with_date)
+app = Flask(__name__)
+client = MongoClient(os.getenv("MONGODB_URI"))
+app.db = client.microblog
+
+@app.route("/", methods=["GET", "POST"])
+def home():
     
-    return app
+    if request.method =="POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%d-%m-%Y")
+        app.db.entries.insert_one({"content":entry_content, "date":formatted_date})
+        #append adiciona contents ao tuple entries
+    entries_with_date=[
+        (entry["content"], entry["date"],
+         datetime.datetime.strptime(entry["date"], "%d-%m-%Y").strftime("%d-%m-%Y"))
+        
+        for entry in app.db.entries.find({})
+        ]
+    return render_template(
+        "home.html", entries = entries_with_date)
+
